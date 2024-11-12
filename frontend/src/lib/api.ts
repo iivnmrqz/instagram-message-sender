0ef909f6e29ef66dac285a38d40f5260acae1a18
Ivan Marquez <iivnmrqz0216@gmail.com>
2024-11-12 19:56:56 +0800

    chore: remove unused files

--- frontend/src/lib/api.ts
+++
@@ -1,6 +0,0 @@
-import type { InstagramMessage } from './types/api'
-import apiClient from './axios'
-
-export const sendInstagramMessage = async (payload: InstagramMessage) => {
-  return apiClient.post('/send-message', payload)
-}
